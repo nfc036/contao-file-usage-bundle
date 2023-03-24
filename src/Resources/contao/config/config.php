7be9f64ee215a4dbd9c1841f008adda873a5b272
nfc036 <steffen.bock@io-warnemuende.de>
2023-03-24 18:02:52 +0100

    Update config.php
    Cron-Aufruf an 4.13 anpassen.

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 // Usage Updater
-$GLOBALS['TL_CRON']['hourly']['updateFileUsages'] = array('Nfc036\ContaoFileUsageBundle\Module\FileUsageHelper', 'updateFileUsages');
+$GLOBALS['TL_CRON']['hourly'][] = [\Nfc036\ContaoFileUsageBundle\Module\FileUsageHelper::class, 'updateFileUsages'];
 $GLOBALS['TL_PURGE']['custom']['updateFileUsages'] = array('callback' => array('Nfc036\ContaoFileUsageBundle\Module\FileUsageHelper', 'updateFileUsages'));
